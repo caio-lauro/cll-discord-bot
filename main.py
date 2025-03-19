3d60b4d879786b0cf11a137678e5bc8dddeca1fe
caio-lauro <caiolaurodelima@gmail.com>
2025-03-19 10:03:59 -0300

    feat: discord bot using discord.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if 'https://x.com/i/status/' in message.content:
+        new_message = f'{message.author.mention}\n{message.content.replace('x.com', 'fxtwitter.com')}'
+        try:
+            await message.delete()
+        except Exception as e:
+            print('Exception occurred.', e, sep='\n')
+        await message.channel.send(new_message)
+
+client.run('TOKEN')
